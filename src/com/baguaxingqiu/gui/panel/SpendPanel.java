e5e5f00b83f6acee9cfa7aab550af351f5fc3b2a
xiongleiwinnie <945058495@qq.com>
2017-12-13 07:49:25 +0800

    花费板块

---
+++ src/com/baguaxingqiu/gui/panel/SpendPanel.java
@@ -0,0 +1,17 @@
+package com.baguaxingqiu.gui.panel;
+
+import com.baguaxingqiu.gui.util.GUIUtil;
+
+import javax.swing.*;
+
+public class SpendPanel extends JPanel{
+    static {
+        GUIUtil.useLNF();
+    }
+
+    public static SpendPanel instance = new SpendPanel();
+
+
+
+
+}
